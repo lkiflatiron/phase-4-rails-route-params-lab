636f0930436e33d6b551f88bf2161c971c70525f
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-13 00:03:13 -0500

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    name=params[:name]
+    
+    if name == nil
+      students = Student.all
+    else
+      name = name.capitalize
+      students = Student.where(first_name: name).or(Student.where(last_name: name))
+    end
+
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
